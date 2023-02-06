09930cde60f8bcdb542b5e0ad1fcc81ae1437d1e
aeron34 <sniperud@gmail.com>
2023-02-05 19:09:25 -0500

    another

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Box, Divider, Drawer, Link, List, ListItem, ListItemButton, ListItemIcon } from '@mui/material';
+import { Box, Divider, Drawer, Link, List, ListItem, ListItemButton, ListItemIcon, Typography } from '@mui/material';
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import TimelineIcon from '@mui/icons-material/Timeline';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import styles from './Home.module.scss';
 
 const sideBarIcons = [
   <FormatListBulletedIcon/>,
@@ -10,34 +12,63 @@ const sideBarIcons = [
 
 const Home = () => {
   return (
-    <Box>
-
+    <Box sx={{
+      paddingLeft: '11rem'
+    }}>
       <Drawer
         sx={{
           '& .MuiDrawer-paper': {
             width: '10rem'
-           // paddingRight: '2rem'
           }
         }}
         variant='permanent' 
         anchor='left'
       >
-      <Box sx={{
-        height: '10rem',
-      }}></Box>
+      <Box className={styles.box}>
+        <div>
+          <AccountCircleIcon
+            fontSize='large'
+          />
+        </div>
+        <div>
+          <Typography>Example Account</Typography>
+        </div>
+      </Box>
       <Divider />
         <List>
-          {sideBarIcons.map(x => (
+          {sideBarIcons.map(icon => (
             <ListItem disablePadding >
               <ListItemButton>
-                <ListItemIcon> {x} </ListItemIcon>
+                <ListItemIcon> {icon} </ListItemIcon>
                 <Link underline="none"> One </Link>
               </ListItemButton>
             </ListItem>
           ))}
         </List>
       </Drawer>
+      <Dash/>
     </Box>
   )
 }
+
+const Dash = () => {
+  return (
+    <Box className={styles.dash}>
+      Dashboard
+      <Box className={styles['header-buttons-bar']}>
+        <div>Grid</div>
+        <div>Grid</div>
+        <div>space</div>
+        <div>Grid</div>
+        <div>Grid</div>
+      </Box>
+      <Box className={styles['header-dash']}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </Box>
+    </Box>
+  );
+}
+
 export default Home;
